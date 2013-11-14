08f0664421730f4940f4a49ae45e2ab1f8b961b8
hansfilipelo <hansfilipelo@gmail.com>
2013-11-14 09:12:38 +0100

    getTime. Call with currantDateTime()

---
+++ server/getTime.cc
@@ -0,0 +1,27 @@
+/*
+FILNAMN: 		getTime.cc
+PROGRAMMERARE:	hanel742, eriek984, jened502, tobgr602, niker917, davha227
+SKAPAD DATUM:	2013-11-14
+BESKRIVNING:	
+*/
+
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <time.h>
+
+using namespace std;
+
+// Get current date/time, format is YYYY-MM-DD.HH:mm:ss
+const std::string currentDateTime() {
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+    // Visit http://en.cppreference.com/w/cpp/chrono/c/strftime
+    // for more information about date/time format
+    strftime(buf, sizeof(buf), "%Y-%m-%d %X", &tstruct);
+    
+    return buf;
+}
