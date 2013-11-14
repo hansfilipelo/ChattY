eec565d43d4e7e003852818ef46f38a8603c1673
hansfilipelo <hansfilipelo@gmail.com>
2013-11-14 14:20:34 +0100

    Changed make clean in makefiles. Now really cleans. Added room_test.cc

---
+++ server/room_test.cc
@@ -0,0 +1,25 @@
+/*
+FILNAMN: 		room_test.cc
+PROGRAMMERARE:	hanel742, eriek984, jened502, tobgr602, niker917, davha227
+SKAPAD DATUM:	2013-11-14
+BESKRIVNING:	
+*/
+
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+#include "getTime.cc"
+#include "message.h"
+
+using namespace std;
+
+int main() {
+    
+    
+    
+    return 0;
+}
