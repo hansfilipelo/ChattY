64b1615228370f5d8ed22132445f88c0b1dd3a98
hansfilipelo <hansfilipelo@gmail.com>
2013-11-20 16:31:10 +0100

    Script for creating class

---
+++ scripts/newClass.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+CLASS=$1
+touch $CLASS.h
+touch $CLASS.cc
+
+cat << _EOF_ > ./$CLASS.h
+/*
+_EOF_
+
+echo "FILNAMN: 		$CLASS.h" >> $CLASS.h
+
+cat << _EOF_ >> ./$CLASS.h
+PROGRAMMERARE:	hanel742, eriek984, jened502, tobgr602, niker917, davha227
+_EOF_
+
+echo "SKAPAD DATUM:	$(date '+%Y-%m-%d')" >> $CLASS.h
+
+cat << _EOF_ >> ./$CLASS.h
+BESKRIVNING:	
+*/
+
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <map>
+
+_EOF_
+
+
+cat << _EOF_ > ./$CLASS.cc
+/*
+_EOF_
+
+echo "FILNAMN: 		$CLASS.cc" >> $CLASS.cc
+
+cat << _EOF_ >> ./$CLASS.cc
+PROGRAMMERARE:	hanel742, eriek984, jened502, tobgr602, niker917, davha227
+_EOF_
+
+echo "SKAPAD DATUM:	$(date '+%Y-%m-%d')" >> $CLASS.cc
+
+cat << _EOF_ >> ./$CLASS.cc
+BESKRIVNING:	
+*/
+
+#include "$CLASS.h"
+
+_EOF_
+
+
+echo "Skapat ny klass $CLASS
+
