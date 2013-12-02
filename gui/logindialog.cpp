78797a11bd98160814934d485bea868867f5a56a
Erik <eriek984@student.liu.se>
2013-12-02 10:13:16 +0100

    added logindialog files

---
+++ gui/logindialog.cpp
@@ -0,0 +1,27 @@
+#include "logindialog.h"
+#include "ui_logindialog.h"
+#include "ui_chatwindow.h"
+#include "chatwindow.h"
+
+LoginDialog::LoginDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::LoginDialog)
+{
+    ui->setupUi(this);
+    this->setFixedSize(this->width(),this->height());
+}
+
+LoginDialog::~LoginDialog()
+{
+    delete ui;
+}
+
+void LoginDialog::on_buttonBox_rejected()
+{
+    this->close();
+}
+
+void LoginDialog::on_buttonBox_accepted()
+{
+
+}
