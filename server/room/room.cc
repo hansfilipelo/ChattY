e803af61d4d796ca64611ae339be200fe9a98712
hansfilipelo <hansfilipelo@gmail.com>
2013-12-16 15:01:51 +0100

    Removed couts

--- server/room/room.cc
+++ server/room/room.cc
@@ -209,7 +209,6 @@ void Room::setFilePath() {
 void Room::readAllFromFile() {
     log.clear();
     string line;
-    cout << filepath << endl;
     ifstream logfile (filepath);
     
     if (logfile.is_open())
